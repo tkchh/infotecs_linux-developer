86ce67d648a668ebe522b4271f2a9843a561b3e8
Alexander <krupkocaca@gmail.com>
2026-08-26 05:25:13 +0700

    Добавил запись в файл с помощью workqueue

--- hello_module.c
+++ hello_module.c
@@ -2,22 +2,55 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/jiffies.h>
+#include <linux/fs.h>
+#include <linux/workqueue.h>
+#include <linux/string.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("TKCHH");
 MODULE_DESCRIPTION("Writing module in file with timer");
 
-static int period_sec = 5;
+static const char msg[] = "Hello from kernel module\n";
+
+static int period_sec = 3;
+static char *target_file = "/tmp/kernel_output.txt";
+
+static struct work_struct write_work;
 static struct timer_list my_timer;
 
+static void write_work_func(struct work_struct *work){
+    struct file *file = NULL;
+    loff_t pos = 0;
+    ssize_t written;
+
+    file = filp_open(target_file, O_WRONLY | O_CREAT | O_APPEND, 0644);
+    if (IS_ERR(file)) {
+        pr_err("Не удалось открыть файл %s: ошибка %ld\n", target_file, PTR_ERR(file));
+        return;
+    }
+
+    pos = file_inode(file)->i_size;
+
+    written = kernel_write(file, msg, strlen(msg), &pos);
+
+    if (written < 0) {
+        pr_err("Ошибка записи: %ld\n", written);
+    }else {
+        pr_info("Записано %ld байт в %s\n", written, target_file);
+    }
+}
+
 static void my_timer_func(struct timer_list *unused) {
-    pr_info("Thats from timer:)");
+    schedule_work(&write_work);
+    pr_info("Added schedule work");
 
     mod_timer(&my_timer, jiffies + msecs_to_jiffies(period_sec * 1000));
 }
 
-static int __init hello_2_init(void) {
-    pr_info("Hello, world 2!\n");
+static int __init hello_init(void) {
+    pr_info("Загружен модуль. Период: %d сек, файл: %s\n", period_sec, target_file);
+
+    INIT_WORK(&write_work, write_work_func);
 
     timer_setup(&my_timer, my_timer_func, 0);
 
@@ -26,10 +59,13 @@ static int __init hello_2_init(void) {
     return 0;
 }
 
-static void __exit hello_2_exit(void) {
+static void __exit hello_exit(void) {
     del_timer_sync(&my_timer);
-    pr_info("Goodbye, world 2!\n");
+
+    cancel_work_sync(&write_work);
+
+    pr_info("Модуль отключен!\n");
 }
 
-module_init(hello_2_init);
-module_exit(hello_2_exit);
+module_init(hello_init);
+module_exit(hello_exit);
